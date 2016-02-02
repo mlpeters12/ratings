8900faab0cc133f18c6944da9a19167b596eca70
Hackbright Student <info@hackbrightacademy.com>
2016-02-01 18:04:31 -0800

    working on datetime from u.item in seed.py

---
+++ seed.py
@@ -0,0 +1,131 @@
+"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+
+from sqlalchemy import func
+from model import User, Movie, Rating
+# from model import Rating
+# from model import Movie
+
+from model import connect_to_db, db
+from server import app
+from datetime import datetime
+
+
+def load_users():
+    """Load users from u.user into database."""
+
+    print "Users"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    User.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.user"):
+        row = row.rstrip()
+        user_id, age, gender, occupation, zipcode = row.split("|")
+
+        user = User(user_id=user_id,
+                    age=age,
+                    zipcode=zipcode)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def load_movies():
+    """Load movies from u.item into database."""
+
+    print "Movies"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Movie.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.item"):
+        row_list = row.rstrip()
+        # movie_id, title, released_at, video_release_date, imdb_url, unknown, action, adventure, animation,
+        # childrens, comedy, crime, documentary, drama, fantasy, filmnoir, horror, musical, romance, scifi, 
+        # thriller, war, western 
+        movie_id = row_list[0]
+        title = row_list[1]
+        released_str = row_list[2]
+        imdb_url = row_list[4]
+
+        if released_str:
+            released_at = datetime.strptime(released_str, "%d-%b-%Y")
+        else:
+            released_at = None
+
+        new_title = title.rstrip(title[-6:])
+
+        print title
+        print new_title
+        print released_at
+
+        movie = Movie(movie_id=movie_id,
+                        title=new_title,
+                        released_at=released_at,
+                        imdb_url=imdb_url)
+
+        # We need to add to the session or it won't ever be stored
+        print movie
+        db.session.add(movie)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def load_ratings():
+    """Load ratings from u.data into database."""
+
+    print "Ratings"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Rating.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        user_id, movie_id, score, timestamp = row.split("\t")
+
+        rating = Rating(movie_id=movie_id,
+                        user_id=user_id,
+                        score=score)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(rating)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+
+def set_val_user_id():
+    """Set value for the next user_id after seeding database"""
+
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(User.user_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next user_id to be max_id + 1
+    query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_users()
+    load_movies()
+    load_ratings()
+    set_val_user_id()
